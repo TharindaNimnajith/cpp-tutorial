64b1beaacb7155fa2d76576c32a0ae28c87d3ce7
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-31 23:54:53 +0530

    getting started

---
+++ cmake-build-debug/CMakeFiles/cpp_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_tutorial.dir/getting-started/io.cpp.obj"
+  "CMakeFiles/cpp_tutorial.dir/getting-started/main.cpp.obj"
+  "CMakeFiles/cpp_tutorial.dir/getting-started/test.cpp.obj"
+  "cpp_tutorial.exe"
+  "cpp_tutorial.exe.manifest"
+  "cpp_tutorial.lib"
+  "cpp_tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ getting-started/io.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ASUS on 10/31/2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int favorite_number;
+    cout << "Enter a number between 1 and 100: ";
+    cin >> favorite_number;
+    cout << "Number: " << favorite_number << endl;
+    return 0;
+}
