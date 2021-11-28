9d8f3c18a2c7ca3ff52e1ee91cac06e5bc4ee5fa
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-11-29 04:21:40 +0530

    date time

--- socket-programming/Client.cpp
+++ socket-programming/Client.cpp
@@ -2,3 +2,9 @@
 // Created by ASUS on 11/29/2021.
 //
 
+#include "Server.cpp"
+
+int main() {
+    getDateTime();
+    return 0;
+}
--- socket-programming/Server.cpp
+++ socket-programming/Server.cpp
@@ -2,3 +2,10 @@
 // Created by ASUS on 11/29/2021.
 //
 
+#include <iostream>
+#include <chrono>
+#include "date.h"
+
+void getDateTime() {
+    std::string s = date::format("%F %T", std::chrono::system_clock::now());
+}
